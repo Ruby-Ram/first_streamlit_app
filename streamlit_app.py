bc83ead2c765538066cfd1ff90be6227af54c054
Ruby-Ram <70371704+Ruby-Ram@users.noreply.github.com>
2023-07-14 19:55:53 +0530

    Update streamlit_app.py
    fixed typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruits_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streatlit.dataframe(my_fruits_list)
+streamlit.dataframe(my_fruits_list)
